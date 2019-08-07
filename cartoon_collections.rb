8e599399b6dfbde375b9b985baaac97cde18cd6d
Nick Telenson <ntelenson@gmail.com>
2019-08-07 21:28:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,8 @@ end
 
 
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include?
 end
