e8a7c45a34d1145bcdef62407be0aea1202a87cc
Nick Telenson <ntelenson@gmail.com>
2019-08-07 21:12:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,9 +10,13 @@ def summon_captain_planet(array)
   array.map{ |i| "#{i.capitalize}!" }
 end
 
+
+
 def long_planeteer_calls(array)
   array.any?{ |word| word.length > 4 }
 end
+
+
 
 def find_the_cheese# code an argument here
   # the array below is here to help
